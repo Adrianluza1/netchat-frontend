c8effa7135ebcd452c029bd0c6e0c9bd30351d87
joseluza <u202213404@upc.edu.pe>
2024-11-03 23:52:10 -0500

    feat(Header): Agregar header de modelo inicial

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,12 @@
+// src/app/shared/components/header/header.component.ts
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  standalone: true,
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  title = 'NetChat';
 }
